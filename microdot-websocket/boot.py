890caee218460dfb45be6cda7d0595b08a36cf70
Olivier Bienz <58851935+lan-ensad@users.noreply.github.com>
2026-01-22 17:36:56 +0100

    add AP mode boot.py

--- microdot-websocket/boot.py
+++ microdot-websocket/boot.py
@@ -1,18 +1,28 @@
 import network, utime
 
-SSID = "domo"
-SSI_PASSWORD = "th1Sp4((!"
+# =====	STA MODE =====
+# SSID = "domo"
+# SSI_PASSWORD = "th1Sp4((!"
+# 
+# def do_connect():
+#     import network
+#     sta_if = network.WLAN(network.STA_IF)
+#     if not sta_if.isconnected():
+#         print('connecting to network...')
+#         sta_if.active(True)
+#         sta_if.connect(SSID, SSI_PASSWORD)
+#         while not sta_if.isconnected():
+#             pass
+#     print('Connected! Network config:', sta_if.ifconfig())
+#     
+# print("Connecting to your wifi...")
+# do_connect()
 
-def do_connect():
-    import network
-    sta_if = network.WLAN(network.STA_IF)
-    if not sta_if.isconnected():
-        print('connecting to network...')
-        sta_if.active(True)
-        sta_if.connect(SSID, SSI_PASSWORD)
-        while not sta_if.isconnected():
-            pass
-    print('Connected! Network config:', sta_if.ifconfig())
-    
-print("Connecting to your wifi...")
-do_connect()
+# ===== AP MODE =====
+AP_SSID = "ESP32-AP"  # Name of the Access Point
+AP_PASSWORD = "12345678"  # Password for the Access Point
+
+ap_if = network.WLAN(network.AP_IF)
+ap_if.active(True)
+ap_if.config(essid=AP_SSID, password=AP_PASSWORD)
+print('Access Point started. Config:', ap_if.ifconfig())
